8b239febad2aa630a3bd433a6559b32a20c26a4c
vatran robert <vatranrobert@Airsomtorvatran.lan>
2022-01-12 09:58:10 +0100

    first commit

---
+++ Farm-Finder/Farm_FinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Farm_FinderApp.swift
+//  Farm-Finder
+//
+//  Created by vatran robert on 2022-01-12.
+//
+
+import SwiftUI
+
+@main
+struct Farm_FinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
